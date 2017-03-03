d09fffe8e5e49f5886806552bea3c3ff32b9b6bb
Steve Barskey <steve@Mac.local>
2017-03-03 07:38:17 -0800

    More updates

--- testapp.py
+++ testapp.py
@@ -3,14 +3,15 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.config import Config
 from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import NumericProperty, StringProperty, ListProperty
-from kivy.uix.screenmanager import Screen
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition, NoTransition
 
 #// Armor Attack
 AASounds = [
-	[ 'Tank Enable',    'ACT_TGL', 0, 'ST_OUT', 0x0001, 0x0001, 1],
-	[ 'Beep Enable',    'ACT_TGL', 0, 'ST_OUT', 0x0001, 0x0001, 2],
-	[ 'Chopper Enable', 'ACT_TGL', 0, 'ST_OUT', 0x0001, 0x0001, 3],
+	[ 'Tank En.',    'ACT_TGL', 0, 'ST_OUT', 0x0001, 0x0001, 1],
+	[ 'Beep En.',    'ACT_TGL', 0, 'ST_OUT', 0x0001, 0x0001, 2],
+	[ 'Chopper En.', 'ACT_TGL', 0, 'ST_OUT', 0x0001, 0x0001, 3],
 	[ 'Tank Fire',      'ACT_PLS', 1, 'ST_AAS', 0x0001, 0x0001, 7],
 	[ 'Hi Explosion',   'ACT_PLS', 1, 'ST_AAS', 0x0001, 0x0001, 6],
 	[ 'Jeep Fire',      'ACT_PLS', 1, 'ST_AAS', 0x0001, 0x0001, 5],
@@ -20,26 +21,26 @@ AASounds = [
 
 #// Rip Off
 ROSounds = [
-	[ 'Explosion',         'ACT_PLS', 0, 'ST_OUT', 0x0001, 0x0001, 7],
+	[ 'Explsn',         'ACT_PLS', 0, 'ST_OUT', 0x0001, 0x0001, 7],
 	[ 'Laser',             'ACT_PLS', 4, 'ST_OUT', 0x0001, 0x0001, 4],
 	[ 'Torpedo',           'ACT_PLS', 3, 'ST_OUT', 0x0001, 0x0001, 3],
-	[ 'Background Speed',  'ACT_SET', 0, 'ST_ROS', 0x0007, 0x0000, 3],
-	[ 'Background Enable', 'ACT_TGL', 0, 'ST_ROS', 0x0001, 0x0001, 2],
+	[ 'BG Speed',  'ACT_SET', 0, 'ST_ROS', 0x0007, 0x0000, 3],
+	[ 'BG En.', 'ACT_TGL', 0, 'ST_ROS', 0x0001, 0x0001, 2],
 	[ 'Beep',              'ACT_TGL', 0, 'ST_ROS', 0x0001, 0x0001, 1],
 	[ 'Motor',             'ACT_TGL', 0, 'ST_ROS', 0x0001, 0x0001, 0]
 ]
 
 #// Solar Quest
 SQSounds = [
-	[ 'Loud Explosion', 'ACT_PLS', 1, 'ST_SQ1', 0x0001, 0x0001,  7],
-	[ 'Soft Explosion', 'ACT_PLS', 1, 'ST_SQ1', 0x0001, 0x0001,  6],
+	[ 'Loud Explsn', 'ACT_PLS', 1, 'ST_SQ1', 0x0001, 0x0001,  7],
+	[ 'Soft Explsn', 'ACT_PLS', 1, 'ST_SQ1', 0x0001, 0x0001,  6],
 	[ 'Thrust',         'ACT_TGL', 0, 'ST_SQ1', 0x0001, 0x0001,  5],
 	[ 'Fire',           'ACT_PLS', 1, 'ST_SQ1', 0x0001, 0x0001,  4],
 	[ 'Capture',        'ACT_PLS', 1, 'ST_SQ1', 0x0001, 0x0001,  3],
 	[ 'Nuke',           'ACT_TGL', 0, 'ST_SQ1', 0x0001, 0x0000,  2],
 	[ 'Photon',         'ACT_PLS', 1, 'ST_SQ1', 0x0001, 0x0001,  1],
-	[ 'Music Enable',   'ACT_TGL', 0, 'ST_SQ2', 0x0001, 0x0000, 15],
-	[ 'Music Volume',   'ACT_SET', 0, 'ST_SQ2', 0x0007, 0x0007, 12],
+	[ 'Music En.',   'ACT_TGL', 0, 'ST_SQ2', 0x0001, 0x0000, 15],
+	[ 'Music Vol',   'ACT_SET', 0, 'ST_SQ2', 0x0007, 0x0007, 12],
 	[ 'Music Pitch',    'ACT_SET', 0, 'ST_SQ2', 0x0FFF, 0x0000,  0]
 ]
 
@@ -49,20 +50,20 @@ SWSounds = [
 	[ 'Fire',         'ACT_PLS', 1, 'ST_OUT', 0x0001, 0x0000, 1],
 	[ 'Thrust 1',     'ACT_TGL', 0, 'ST_OUT', 0x0001, 0x0001, 2],
 	[ 'Thrust 2',     'ACT_TGL', 0, 'ST_OUT', 0x0001, 0x0001, 3],
-	[ 'Sound Enable', 'ACT_TGL', 0, 'ST_OUT', 0x0001, 0x0001, 4]
+	[ 'Sound En.', 'ACT_TGL', 0, 'ST_OUT', 0x0001, 0x0001, 4]
 ]
 
 #// Star Castle
 SCSounds = [
-	[ 'Loud Explosion',    'ACT_PLS', 1, 'ST_OUT', 0x0001, 0x0001, 1],
-	[ 'Soft Explosion',    'ACT_PLS', 1, 'ST_OUT', 0x0001, 0x0001, 2],
-	[ 'Laser Enable',      'ACT_TGL', 0, 'ST_OUT', 0x0001, 0x0001, 3],
-	[ 'Fire Ball Enable',  'ACT_TGL', 0, 'ST_AAS', 0x0001, 0x0001, 7],
-	[ 'Shield Enable',     'ACT_TGL', 0, 'ST_AAS', 0x0001, 0x0001, 6],
-	[ 'Star Enable',       'ACT_TGL', 0, 'ST_AAS', 0x0001, 0x0000, 5],
-	[ 'Thrust Enable',     'ACT_TGL', 0, 'ST_AAS', 0x0001, 0x0001, 4],
-	[ 'Background Enable', 'ACT_TGL', 0, 'ST_AAS', 0x0001, 0x0001, 3],
-	[ 'Background Speed',  'ACT_SET', 0, 'ST_AAS', 0x0007, 0x0000, 0]
+	[ 'Loud Explsn',    'ACT_PLS', 1, 'ST_OUT', 0x0001, 0x0001, 1],
+	[ 'Soft Explsn',    'ACT_PLS', 1, 'ST_OUT', 0x0001, 0x0001, 2],
+	[ 'Laser En.',      'ACT_TGL', 0, 'ST_OUT', 0x0001, 0x0001, 3],
+	[ 'Fire Ball En.',  'ACT_TGL', 0, 'ST_AAS', 0x0001, 0x0001, 7],
+	[ 'Shield En.',     'ACT_TGL', 0, 'ST_AAS', 0x0001, 0x0001, 6],
+	[ 'Star En.',       'ACT_TGL', 0, 'ST_AAS', 0x0001, 0x0000, 5],
+	[ 'Thrust En.',     'ACT_TGL', 0, 'ST_AAS', 0x0001, 0x0001, 4],
+	[ 'BG En.', 'ACT_TGL', 0, 'ST_AAS', 0x0001, 0x0001, 3],
+	[ 'BG Speed',  'ACT_SET', 0, 'ST_AAS', 0x0007, 0x0000, 0]
 ]
 
 #// Star Hawk
@@ -79,7 +80,7 @@ SHSounds = [
 SDSounds = [
 	[ 'Bong',      'ACT_PLS', 1, 'ST_OUT', 0x0001, 0x0001, 0],
 	[ 'Whoosh',    'ACT_PLS', 0, 'ST_OUT', 0x0001, 0x0001, 1],
-	[ 'Explosion', 'ACT_PLS', 0, 'ST_OUT', 0x0001, 0x0001, 2],
+	[ 'Explsn', 'ACT_PLS', 0, 'ST_OUT', 0x0001, 0x0001, 2],
 	[ 'Ping 1',    'ACT_PLS', 1, 'ST_OUT', 0x0001, 0x0001, 3],
 	[ 'Ping 2',    'ACT_PLS', 1, 'ST_OUT', 0x0001, 0x0001, 4],
 	[ 'Hatch',     'ACT_PLS', 6, 'ST_OUT', 0x0001, 0x0001, 7]
@@ -87,7 +88,7 @@ SDSounds = [
 
 #// Tailgunner
 TGSounds = [
-	[ 'Explosion',     'ACT_PLS',  0, 'ST_TGS', 0x0001, 0x0001, 0],
+	[ 'Explsn',     'ACT_PLS',  0, 'ST_TGS', 0x0001, 0x0001, 0],
 	[ 'Rumble',        'ACT_TGL',  0, 'ST_TGS', 0x0001, 0x0001, 1],
 	[ 'Laser',         'ACT_TGR',  4, 'ST_TGS', 0x0001, 0x0001, 2],
 	[ 'Shield',        'ACT_TGL',  0, 'ST_TGS', 0x0001, 0x0001, 3],
@@ -106,6 +107,17 @@ SoundBoard = [
 	[ 'Tailgunner',   len(TGSounds), TGSounds]
 ]
 
+GameBoards = {
+	'Armor Attack': AASounds,
+	'Rip Off': ROSounds,
+	'Solar Quest': SQSounds,
+	'Space Wars': SWSounds,
+	'Star Castle': SCSounds,
+	'Star Hawk': SHSounds,
+	'Sundance': SDSounds,
+	'Tailgunner': TGSounds
+}
+
 blue = [0.33, 0.5, 1, 1]
 #'dkblue': [0.15, 0.33, 0.5, 1]
 
@@ -115,32 +127,39 @@ Config.set('graphics', 'height', '320')
 class GameButton(Button):
 	pass
 
-class MainScreen(Screen):
-	pass
-	
-class SoundsScreen(Screen):
+class SoundButton(Button):
 	pass
 
-	def on_enter(self):
+class MainMenu(Screen):
+	pass
+
+class Sounds(Screen):
+	game = None
+
+	def on_pre_enter(self):
 		self.ids.btnstack.clear_widgets()
 		id = 0
-		for sound in SoundBoard[0][2]:
-			btn = GameButton()
+
+		for sound in GameBoards[self.game]:
+			btn = SoundButton()
 			btn.text = sound[0]
 			btn.id = str(id)
 			self.ids.btnstack.add_widget(btn)
 			id += 1
-	
-	def play_sound(soundid):
-		print soundid
 
-class AdvSoundsScreen(Screen):
+	def set_gamename(self, gamename):
+		self.game = gamename
+
+	def play_sound(self):
+		print sound
+
+class AdvSounds(Screen):
 	pass
 
-class GamesScreen(Screen):
-	pass
+class Games(Screen):
+	game = StringProperty('')
 
-	def on_enter(self):
+	def on_pre_enter(self):
 		self.ids.btnstack.clear_widgets()
 		id = 0
 		for game in SoundBoard:
@@ -150,58 +169,34 @@ class GamesScreen(Screen):
 			self.ids.btnstack.add_widget(btn)
 			id += 1
 
-class MyScreen(Screen):
-	#fullscreen = BooleanProperty(False)
+cat = Builder.load_file('cat.kv')
 
-	def add_widget(self, *args):
-		if 'btnstack' in self.ids:
-			return self.ids.btnstack.add_widget(*args)
-		return super(MyScreen, self).add_widget(*args)
-			
+sm = ScreenManager()
+sm.add_widget(MainMenu(name='MainMenu'))
+sm.add_widget(Games(name='Games'))
+sm.add_widget(Sounds(name='Sounds'))
+sm.add_widget(AdvSounds(name='AdvSounds'))
+
+cat.add_widget(sm)
+
 class CATApp(App):
 
-	index = NumericProperty(-1)
-	current_title = StringProperty()
-	sourcecode = StringProperty()
-	screen_names = ListProperty([])
-	hierarchy = ListProperty([])
-
 	def build(self):
-		self.title = 'hello world'
-		self.screens = {}
-		self.available_screens = [
-			'Main', 'Games', 'Sounds', 'AdvSounds'
-		]
-		self.screen_names = self.available_screens
-		curdir = dirname(__file__)
-		self.available_screens = [join(curdir, 'data', 'screens',
-			'{}.kv'.format(fn)) for fn in self.available_screens]
-		self.go_next_screen()
+		return cat
 
-	def go_previous_screen(self):
-		self.index = (self.index - 1) % len(self.available_screens)
-		screen = self.load_screen(self.index)
-		sm = self.root.ids.sm
-		sm.switch_to(screen, direction='right')
-		self.current_title = screen.name
+	def go_screen(self, screen, dir):
+		if dir == 'None':
+			sm.transition = NoTransition()
+		else:
+			sm.transition = SlideTransition()
+			sm.transition.direction = dir
+		sm.current = screen
 
-	def go_next_screen(self):
-		self.index = (self.index + 1) % len(self.available_screens)
-		screen = self.load_screen(self.index)
-		sm = self.root.ids.sm
-		sm.switch_to(screen, direction='left')
-		self.current_title = screen.name
+	def set_selected_game(self, gamename):
+		sm.screens[2].game = gamename
 
-	def go_screen(self, idx):
-		self.index = idx
-		self.root.ids.sm.switch_to(self.load_screen(idx), direction='left')
-
-	def load_screen(self, index):
-		if index in self.screens:
-			return self.screens[index]
-		screen = Builder.load_file(self.available_screens[index].lower())
-		self.screens[index] = screen
-		return screen
+	def play_sound(self, soundname):
+		print soundname
 
 if __name__ == '__main__':
 	CATApp().run()
